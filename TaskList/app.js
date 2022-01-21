42fdf922804bd46c14f21b24a5871b77adad9d52
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2022-01-21 20:11:21 +0600

    Enhancement #2: Created Delete Task Item

--- TaskList/app.js
+++ TaskList/app.js
@@ -12,6 +12,8 @@ loadEventListeners();
 function loadEventListeners() {
     // add task event
     form.addEventListener('submit', addTask);
+    // Remove task event
+    taskList.addEventListener('click', removeTask);
 }
 
 // Add Task
@@ -43,4 +45,13 @@ function addTask(e) {
 
 
     e.preventDefault();
+}
+
+// Remove Task
+function removeTask(e) {
+    if(e.target.parentElement.classList.contains('delete-item')) {
+        if(confirm('Are You Sure?')) {
+            e.target.parentElement.parentElement.remove();
+        }
+    }
 }

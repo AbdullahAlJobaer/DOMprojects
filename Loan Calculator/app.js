21e0307cac8b8d3823ab743db9927792f01dce7a
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2022-01-26 20:52:32 +0600

    Enhancement #5: Created the basic functionalities
    Added event listener for Calculate button. Created function to calculate results. Declared the UI variables. Created monthly payment functionality.

---
+++ Loan Calculator/app.js
@@ -0,0 +1,26 @@
+// Listen for submit
+document.getElementById('loan-form').addEventListener('submit', calculateResults);
+
+// Calculate Results
+function calculateResults(e) {
+    console.log('Calculating....');
+    // UI variables
+    const amount = document.getElementById('amount');
+    const interest = document.getElementById('interest');
+    const years = document.getElementById('years');
+    const monthlyPayment = document.getElementById('monthly-payment');
+    const totalPayment = document.getElementById('total-payment');
+    const totalInterest = document.getElementById('total-interest');
+
+    const principal = parseFloat(amount.value);
+    const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+    const calculatedPayments = parseFloat(years.value) * 12;
+
+    // Compute monthly payment
+    const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+    const monthly = (principal*x*calculatedInterest)/(x-1);
+
+    
+
+    e.preventDefault();
+}

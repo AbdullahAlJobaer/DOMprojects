ed54217deacd01574fc16ddd85e02bc1c8a4d082
RobJobaer <65289483+AbdullahAlJobaer@users.noreply.github.com>
2022-01-31 22:46:55 +0600

    Enhancement #8: Declared Game Values and UI Elements

---
+++ Number Guesser/app.js
@@ -0,0 +1,22 @@
+/*
+GAME FUNCTION:
+- Player must guess a number between a min and max.
+- Player gets a certain amount of guesses
+- Notify player of guesses remaining
+- Notify the player of the correct answer if loose
+- Let player choose to play again
+*/
+
+// Game Values
+let min = 1,
+    max = 10,
+    winningNum = 2,
+    guessesLeft = 3;
+
+// UI Elements
+const game = document.querySelector('#game'),
+      minNum = document.querySelector('.min-num'),
+      maxNum = document.querySelector('.max-num'),
+      guessBtn = document.querySelector('#guess-btn'),
+      guessInput = document.querySelector('#guess-input'),
+      message = document.querySelector('.message');
